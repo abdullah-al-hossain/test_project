f364d80ede04a464c4b0730aec5cdc5f2af64c7b
abdullah-al-hossain <abdullahcuet14cse@gmail.com>
2024-05-18 13:22:04 +0600

    delete already existing file using and store new one

--- app/Http/Controllers/Profile/AvatarController.php
+++ app/Http/Controllers/Profile/AvatarController.php
@@ -5,12 +5,17 @@ namespace App\Http\Controllers\Profile;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Requests\UpdateAvatarRequest;
+use Illuminate\Support\Facades\Storage;
 
 class AvatarController extends Controller
 {
     public function update(UpdateAvatarRequest $request)
     {
         $path = $request->file('avatar')->store('avatars', 'public');
+
+        if($oldAvatar = $request->user()->avatar) {
+            Storage::disk('public')->delete($oldAvatar);
+        }
 
         auth()->user()->update([
             'avatar' => $path,
